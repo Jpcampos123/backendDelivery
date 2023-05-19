d535d7b51f8c32eb9417777cf7507921bb76fc17
jpcamposgda <jpcamposgda@gmail.com>
2023-05-19 00:07:34 -0400

    first

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  isNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minNumbers: 0,
+    minLowercase: 0,
+    minUppercase: 0,
+    minSymbols: 0,
+  })
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(data: CreateUserDTO) {
+    return await this.prisma.users.create({
+      data,
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
