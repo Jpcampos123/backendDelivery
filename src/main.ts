bfa6dd5700b716f9232608c2b069f2e03c7b64ab
jpcamposgda <jpcamposgda@gmail.com>
2023-07-03 17:29:13 -0400

    teste10

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
