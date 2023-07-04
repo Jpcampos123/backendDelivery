aadf052ef833eb1617227a0f208cd89bb8d0a1e6
jpcamposgda <jpcamposgda@gmail.com>
2023-07-04 11:35:47 -0400

    cors true

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module';
 
 import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+
+  app.enableCors(corsOptions);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
