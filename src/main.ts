d9e5869ceb98ac27b8284a2f8e4dee24cff1e257
jpcamposgda <jpcamposgda@gmail.com>
2023-07-04 17:16:45 -0400

    cors enable

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.set('access-control-allow-origin', '*');
-    res.set('access-control-allow-methods', '*');
-    res.set('access-control-allow-headers', '*');
-    next();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
-
-  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
